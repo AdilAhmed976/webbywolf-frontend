97c7b3c7cd8b7495a022423aac2e3eccda53799d
AdilAhmed976 <adilahmedah976@gmail.com>
2025-06-18 02:56:42 +0530

    feat : animation added in the bike gallery

--- src/sections/bike-gallery-section.jsx
+++ src/sections/bike-gallery-section.jsx
@@ -1,3 +1,4 @@
+import AnimatedCard from "@/components/animation/animated-card";
 import { Button } from "@/components/ui/button";
 import { Typography } from "@/components/ui/typography";
 import Image from "next/image";
@@ -24,35 +25,43 @@ const BikeGallerySection = () => {
         <div className="grid grid-cols-2 md:grid-cols-3 gap-4 w-full">
           <div className="h-full w-full col-span-1" />
           <div className="h-full w-full col-span-1" />
+
           <div className="h-full w-full col-span-1">
-            <Image
-              src="https://webbywolf-frontend.vercel.app/images/gallery-thumb-1.png"
-              height={200}
-              width={200}
-              className="w-full h-full object-cover"
-              alt="bike1"
-              priority
-            />
+            <AnimatedCard once={true} position="top" index={0}>
+              <Image
+                src="https://webbywolf-frontend.vercel.app/images/gallery-thumb-1.png"
+                height={200}
+                width={200}
+                className="w-full h-full object-cover"
+                alt="bike1"
+                priority
+              />
+            </AnimatedCard>
           </div>
+
           <div className="h-full w-full col-span-1">
-            <Image
-              src="https://webbywolf-frontend.vercel.app/images/gallery-thumb-2.png"
-              height={200}
-              width={200}
-              className="w-full h-full object-cover"
-              alt="bike2"
-              priority
-            />
+            <AnimatedCard once={true} position="top" index={1}>
+              <Image
+                src="https://webbywolf-frontend.vercel.app/images/gallery-thumb-2.png"
+                height={200}
+                width={200}
+                className="w-full h-full object-cover"
+                alt="bike2"
+                priority
+              />
+            </AnimatedCard>
           </div>
           <div className="h-full w-full col-span-2">
-            <Image
-              src="https://webbywolf-frontend.vercel.app/images/gallery-thumb-3.png"
-              height={200}
-              width={200}
-              className="w-full h-full object-cover"
-              alt="bike3"
-              priority
-            />
+            <AnimatedCard once={true} position="bottom" index={2}>
+              <Image
+                src="https://webbywolf-frontend.vercel.app/images/gallery-thumb-3.png"
+                height={200}
+                width={200}
+                className="w-full h-full object-cover"
+                alt="bike3"
+                priority
+              />
+            </AnimatedCard>
           </div>
         </div>
       </div>
@@ -60,54 +69,64 @@ const BikeGallerySection = () => {
       {/* Right Section: Responsive Grid of Individual Images */}
       <div className="grid grid-cols-2 md:grid-cols-4 gap-4 w-full">
         <div className="h-full w-full col-span-1 md:pt-6">
-          <Image
-            src="https://webbywolf-frontend.vercel.app/images/gallery-thumb-4.png"
-            height={200}
-            width={200}
-            className="w-full h-full object-cover"
-            alt="bike4"
-            priority
-          />
+          <AnimatedCard once={true} position="bottom" index={3}>
+            <Image
+              src="https://webbywolf-frontend.vercel.app/images/gallery-thumb-4.png"
+              height={200}
+              width={200}
+              className="w-full h-full object-cover"
+              alt="bike4"
+              priority
+            />
+          </AnimatedCard>
         </div>
         <div className="h-full w-full col-span-3">
-          <Image
-            src="https://webbywolf-frontend.vercel.app/images/gallery-thumb-5.png"
-            height={200}
-            width={200}
-            className="w-full h-[240px] object-cover"
-            alt="bike5"
-            priority
-          />
+          <AnimatedCard once={true} position="top" index={3}>
+            <Image
+              src="https://webbywolf-frontend.vercel.app/images/gallery-thumb-5.png"
+              height={200}
+              width={200}
+              className="w-full h-[240px] object-cover"
+              alt="bike5"
+              priority
+            />
+          </AnimatedCard>
         </div>
         <div className="h-full w-full col-span-4">
-          <Image
-            src="https://webbywolf-frontend.vercel.app/images/gallery-thumb-6.png"
-            height={400}
-            width={400}
-            className="w-full h-full object-cover"
-            alt="bike6"
-            priority
-          />
+          <AnimatedCard once={true} position="right" index={3}>
+            <Image
+              src="https://webbywolf-frontend.vercel.app/images/gallery-thumb-6.png"
+              height={400}
+              width={400}
+              className="w-full h-full object-cover"
+              alt="bike6"
+              priority
+            />
+          </AnimatedCard>
         </div>
         <div className="h-full w-full col-span-1">
-          <Image
-            src="https://webbywolf-frontend.vercel.app/images/gallery-thumb-7.png"
-            height={200}
-            width={200}
-            className="w-full h-full object-cover"
-            alt="bike7"
-            priority
-          />
+          <AnimatedCard once={true} position="bottom" index={3}>
+            <Image
+              src="https://webbywolf-frontend.vercel.app/images/gallery-thumb-7.png"
+              height={200}
+              width={200}
+              className="w-full h-full object-cover"
+              alt="bike7"
+              priority
+            />
+          </AnimatedCard>
         </div>
         <div className="h-full w-full col-span-3">
-          <Image
-            src="https://webbywolf-frontend.vercel.app/images/gallery-thumb-2.png"
-            height={200}
-            width={200}
-            className="w-full h-full object-cover"
-            alt="bike8"
-            priority
-          />
+          <AnimatedCard once={true} position="left" index={3}>
+            <Image
+              src="https://webbywolf-frontend.vercel.app/images/gallery-thumb-2.png"
+              height={200}
+              width={200}
+              className="w-full h-full object-cover"
+              alt="bike8"
+              priority
+            />
+          </AnimatedCard>
         </div>
       </div>
     </section>
