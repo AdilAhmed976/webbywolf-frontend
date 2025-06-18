11af9af8a79f46666bcf9d50627da4a674a057cb
AdilAhmed976 <adilahmedah976@gmail.com>
2025-06-18 17:54:24 +0530

    fix : header UI fix

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -3,9 +3,10 @@
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
-import { ArrowUpFromDotIcon, Menu, Zap } from "lucide-react";
+import { ArrowUpFromDotIcon, ChevronDownIcon, Menu, Zap } from "lucide-react";
 import { useEffect, useState } from "react";
 import clsx from "clsx";
+import { Typography } from "./ui/typography";
 
 const navLinks = [
   {
@@ -13,9 +14,21 @@ const navLinks = [
     href: "/features",
     items: [{ label: "Features", href: "/features" }],
   },
-  { label: "Testimonials", href: "/testimonials", items: [] },
-  { label: "Pricing", href: "/pricing", items: [] },
-  { label: "Contact", href: "/contact", items: [] },
+  {
+    label: "Testimonials",
+    href: "/testimonials",
+    items: [{ label: "Features", href: "/features" }],
+  },
+  {
+    label: "Pricing",
+    href: "/pricing",
+    items: [{ label: "Features", href: "/features" }],
+  },
+  // {
+  //   label: "Contact",
+  //   href: "/contact",
+  //   items: [{ label: "Features", href: "/features" }],
+  // },
 ];
 export default function Header() {
   const [scrolled, setScrolled] = useState(false);
@@ -47,18 +60,28 @@ export default function Header() {
           </Link>
 
           {/* Desktop Navigation */}
-          <nav className="hidden md:flex items-center gap-6">
+          <nav className="hidden md:flex items-center gap-6 text-link">
             {navLinks.map((link) =>
               Array.isArray(link?.items) && link?.items.length > 0 ? (
-                <div key={link.href}>Options</div>
-              ) : (
-                <Link
+                <div
                   key={link.href}
-                  href={link.href}
-                  className="text-sm font-medium hover:underline underline-offset-4"
+                  className="flex items-center gap-2 hover:underline"
                 >
-                  {link.label}
-                </Link>
+                  <Typography type="p" className={"text-link"}>
+                    {link.label}
+                  </Typography>
+                  <ChevronDownIcon className="size-4" />
+                </div>
+              ) : (
+                <Typography type="p" className={"text-link"}>
+                  <Link
+                    key={link.href}
+                    href={link.href}
+                    className="text-sm font-medium hover:underline underline-offset-4"
+                  >
+                    {link.label}
+                  </Link>
+                </Typography>
               )
             )}
           </nav>
@@ -104,7 +127,9 @@ export default function Header() {
       {scrolled && (
         <div className="fixed right-[10px] bottom-[10px] md:right-[30px] md:bottom-[30px] z-50 transition-all duration-300">
           <Button
-            className={"border rounded-full transition-all duration-300 h-10 w-10 shadow-[0px_4px_40px_0px_#fff]"}
+            className={
+              "border rounded-full transition-all duration-300 h-10 w-10 shadow-[0px_4px_40px_0px_#fff]"
+            }
             onClick={() => {
               window.scrollTo({
                 top: 0,
