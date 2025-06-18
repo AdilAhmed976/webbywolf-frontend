026a53ae2adbf532a31dc7f1a9b81bd2c65c4c76
AdilAhmed976 <adilahmedah976@gmail.com>
2025-06-18 14:08:31 +0530

    fix : blog section UI fix

--- src/sections/blog-section.jsx
+++ src/sections/blog-section.jsx
@@ -41,17 +41,21 @@ const blogPosts = [
 const BlogSection = () => {
   return (
     <div className="max-w-7xl mx-auto px-6 sm:px-10 md:px-15 lg:px-20 py-6 sm:py-10 md:py-15 lg:py-20 container">
-      <Typography type="label">Lorem ipsum dolor sit amet</Typography>
-      <Typography type="h2">LOREM IPSUM DOLOR SIT</Typography>
+      <div className="space-y-4 my-4">
+        <Typography type="h5" className={"text-primary"}>
+          Lorem ipsum dolor sit amet
+        </Typography>
+        <Typography type="h2">LOREM IPSUM DOLOR SIT</Typography>
 
-      <p className="text-gray-700 max-w-2xl mb-10">
-        Lorem ipsum dolor sit amet consectetur. Amet sodales sodales facilisis
-        donec dui. Mi porttitor ut id diam in mattis maecenas eget integer in
-        nam. Non nisl faucibus et felis aliquet. Hendrerit felis ut purus
-        lectus.
-      </p>
+        <Typography className="text-gray-900 max-w-2xl">
+          Lorem ipsum dolor sit amet consectetur. Amet sodales sodales facilisis
+          donec dui. Mi porttitor ut id diam in mattis maecenas eget integer in
+          nam. Non nisl faucibus et felis aliquet. Hendrerit felis ut purus
+          lectus.
+        </Typography>
+      </div>
 
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 gap-6 md:gap-10 lg:gap-16 px-0 sm:px-5 md:px-10 lg:px-20">
+      <div className="max-w-5xl mx-auto w-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 gap-6 md:gap-10 lg:gap-16 px-0 sm:px-5 md:px-10 lg:px-20 py-5 sm:py-5 md:py-10 lg:py-15">
         {blogPosts.map((post, idx) => {
           return (
             <AnimatedCard
@@ -60,7 +64,7 @@ const BlogSection = () => {
             >
               <div
                 key={post.id}
-                className={`border rounded-lg transition-all duration-300`}
+                className={`bg-white border rounded-lg transition-all duration-300`}
               >
                 <Image
                   height={200}
@@ -70,7 +74,7 @@ const BlogSection = () => {
                   className="w-full h-[205px] object-cover rounded-md mb-4"
                   priority
                 />
-                <div className="p-4">
+                <div className="p-4 pb-20">
                   <Typography className="text-lg font-semibold mb-2">
                     {post.title}
                   </Typography>
